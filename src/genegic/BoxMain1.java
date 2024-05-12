56681e98f6ffc2f79766665d3f5a86f1c76b32c7
PeachCoolPiece <121959157+PeachCoolPiece@users.noreply.github.com>
2024-05-12 16:42:51 +0900

    다형성을 통한 중복 해결 시도

---
+++ src/genegic/BoxMain1.java
@@ -0,0 +1,20 @@
+package genegic;
+
+import genegic.ex1.IntegerBox;
+import genegic.ex1.StringBox;
+
+public class BoxMain1 {
+    
+    public static void main(String[] args) {
+        IntegerBox integerBox = new IntegerBox();
+        integerBox.setValue(10); // 오토박싱
+        Integer value = integerBox.getValue();
+        System.out.println("value = " + value);
+        
+        StringBox stringBox = new StringBox();
+        stringBox.set("hello");
+        String str = stringBox.get();
+        System.out.println("str = " + str);
+        
+    }
+}
