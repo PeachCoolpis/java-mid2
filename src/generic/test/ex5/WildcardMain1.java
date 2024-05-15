4eb0a3e292e6a9c9f73e9e692a50a32a8975e955
PeachCoolpis <rlglxh@naver.com>
2024-05-15 12:59:49 +0900

    와일드카드1

---
+++ src/generic/test/ex5/WildcardMain1.java
@@ -0,0 +1,26 @@
+package generic.test.ex5;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class WildcardMain1 {
+    public static void main(String[] args) {
+        Box<Object> objectBox = new Box<>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+        
+        dogBox.set(new Dog("멍멍이",100));
+        
+        WildcardEx.printGenericV1(dogBox);
+        WildcardEx.printGenericV2(dogBox);
+        Dog dog = WildcardEx.printAndReturnGeneric(dogBox);
+        
+        WildcardEx.printWildcardV1(dogBox); // 와일드 카드
+        WildcardEx.printWildV2(dogBox);
+        Animal animal = WildcardEx.printAndReturnWildcard(dogBox);
+        
+    }
+    
+    
+}
