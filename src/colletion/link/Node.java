1b7d9a4febbaa0f4525303311568d75e9a38c4d3
PeachCoolpis <rlglxh@naver.com>
2024-07-01 22:08:28 +0900

    노드 와 연결2

--- src/colletion/link/Node.java
+++ src/colletion/link/Node.java
@@ -9,4 +9,28 @@ public class Node {
         this.item = item;
         this.next = null;
     }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append("->");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+    // Ide 생성 toString
+//    @Override
+//    public String toString() {
+//        return "Node{" +
+//                "item=" + item +
+//                ", next=" + next +
+//                '}';
+//    }
 }
