2c35f6cb65c45d6db3c1270dcd0b89e6e8287354
PeachCoolpis <rlglxh@naver.com>
2024-07-02 10:48:17 +0900

    리스트 추상화1 - 인터페이스 도입

---
+++ src/colletion/list/MyArrayList.java
@@ -0,0 +1,121 @@
+package colletion.list;
+
+import java.util.Arrays;
+
+public class MyArrayList<E> implements MyList<E>{
+    
+    private static final int DEFAULT_CAPACITY = 5; // 기본 수용 용량
+    private Object[] elements; // 모든 오브젝트를 담을수 있는 멤버변수 선언
+    private int size = 0;
+    
+    public MyArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+    }
+    
+    public MyArrayList(int initialCapacity) {
+        // 타입 이레이저 때문에 다음과 같은 코드는 불가능합니다:
+        // this.elements = new E[initialCapacity];
+        // 제네릭 타입 정보는 컴파일 타임에만 존재하고 런타임에는 제거되기 때문에
+        // new E[]를 사용하여 배열을 생성할 수 없습니다. 대신 Object[]를 사용합니다.
+        this.elements = new Object[initialCapacity];
+    }
+    @Override
+    public int size() {
+        return size;
+    }
+    @Override
+    public void add(E o) {
+        //코드 추가
+        if (size == elements.length) {
+            grow();
+        }
+        this.elements[size] = o;
+        this.size++;
+    }
+    @Override
+    public void add( int index,E o) {
+        if (size == elements.length) {
+            grow();
+        }
+        // 데이터 이동
+        shiftRightFrom(index);
+        this.elements[index] = o;
+        this.size++;
+    }
+    // 이상한 메서드를 추가한경우
+    public void addBad(String o) {
+        if (size == elements.length) {
+            grow();
+        }
+        // 데이터 이동
+        this.elements[size] = o;
+        this.size++;
+    }
+    
+    //코드 추가, 요소의 마지막부터 index까지 오른쪽으로 밀기
+    private void shiftRightFrom(int index) {
+        for (int i = size; i > index; i--) {
+            this.elements[i] = this.elements[i - 1];
+        }
+    }
+    
+    //코드 추가
+    @Override
+    public E remove(int index) {
+        E oldValue = get(index);
+        //데이터 이동
+        shiftLeftFrom(index);
+        this.size--;
+        this.elements[this.size] = null;
+        return oldValue;
+    }
+    
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i < size - 1; i++) {
+            this.elements[i] = elements[i + 1];
+        }
+    }
+    
+    //코드 추가
+    private void grow() {
+        int oldCapacity = elements.length;
+        int newCapacity = oldCapacity * 2;
+        //배열을 새로 만들고 , 기존 배열을 새로운 배열에 복사
+        this.elements = Arrays.copyOf(elements, newCapacity);
+    }
+    @SuppressWarnings("unchecked") // 어노테이션 주석 , 경고를 없애줌
+    @Override
+    public E get(int index) {
+        return (E) elements[index];
+    }
+    @Override
+    public E set(int index, E o) {
+        E oldValue = get(index);
+        elements[index] = o;
+        return oldValue;
+    }
+    @Override
+    public int indexOf(E o) {
+        if (o == null) {
+            return -1;
+        }
+        for (int i = 0; i < size; i++) {
+            if (o.equals(this.elements[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public String toString() {
+        String format = String.format(" size = %s , capacity = %s", size, this.elements.length);
+        return Arrays.toString(Arrays.copyOf(elements, size)) + format;
+    }
+    
+    public void clear() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+    
+    
+}
